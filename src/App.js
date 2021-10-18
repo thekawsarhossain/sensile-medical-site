ead7cb314dc6b19c4c0c0fa11e4589fa62c4a86d
Kawsar-Hossain-318 <thekawsarhossain@gmail.com>
2021-10-18 18:33:30 +0600

    all set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <p className="bg-yellow-200 text-4xl hover:text-blue-900">This is my text</p>
-      <p className="text-primary">hello my text</p>
-      <p className="text-custom">This is my own tailwind class</p>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2 className="w-28 bg-gray-100">whats upp there </h2>
+        </div>
+    );
+};
+
+export default Header;
